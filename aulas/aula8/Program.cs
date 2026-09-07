c4d20d444f6719c0c6432b9e02482fdb790bf0cf
JoaoGabini <joaogabini03@gmail.com>
2026-09-07 19:51:47 -0300

    terceiro dia

---
+++ aulas/aula8/Program.cs
@@ -0,0 +1,29 @@
+﻿//IF, ELSE IF E ELSE
+
+Console.Write("Digite sua Idade: ");
+int idade= Convert.ToInt32(Console.ReadLine());
+
+
+if (idade >= 18)
+{
+    Console.WriteLine("Acesso Autorizado");
+}else if (idade == 17)
+{
+    Console.WriteLine("Pode entrar com permissão dos pais!");
+    Console.Write("Tem Permissão dos Pais (Para adolescentes de 17 anos apenas!): ");
+    string permissao;
+    permissao= Console.ReadLine();
+    
+    if (permissao == "sim")
+    {
+        Console.WriteLine("Pode entrar!");
+    }
+    else
+    {
+        Console.WriteLine("Não pode Entrar!");
+    }
+}
+else
+{
+    Console.WriteLine("Acesso Negado");
+}
---
+++ aulas/aula9/Program.cs
@@ -0,0 +1,62 @@
+﻿//Switch
+Console.Write ("Diga um dia da semana: ");
+int dia;
+dia= Convert.ToInt32(Console.ReadLine());
+
+//switch substitui esse if enorme
+
+/*if (dia == 1)
+{
+    Console.WriteLine("É Domingo!");
+}else if (dia == 2){
+    Console.WriteLine("É Segunda!");
+}else if (dia == 3){
+    Console.WriteLine("É Terça!");
+}else if (dia == 4){
+    Console.WriteLine("É Quarta!");
+}else if (dia == 5){
+    Console.WriteLine("É Quinta!");
+}else if (dia == 6){
+    Console.WriteLine("É Sexta!");
+}else if (dia == 7){
+    Console.WriteLine("É Sábado!");
+}
+else
+{
+    Console.WriteLine("Dia Inválido!");
+}*/
+
+switch (dia)
+{
+    case 1:
+        Console.WriteLine("É Domingo!");
+        break;
+    
+    case 2:
+        Console.WriteLine("É Segunda!");
+        break;
+    
+    case 3:
+        Console.WriteLine("É Terça!");
+        break;
+    
+    case 4:
+        Console.WriteLine("É Quarta!");
+        break;
+    
+    case 5:
+        Console.WriteLine("É Quinta!");
+        break;
+    
+    case 6:
+        Console.WriteLine("É Sexta!");
+        break;
+    
+    case 7:
+        Console.WriteLine("É Sábado!");
+        break;
+    
+    default:
+        Console.WriteLine("Dia Inválido!");
+        break;
+}
